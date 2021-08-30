a0e77203f0285bd02ebc4261021d56a36c44a94a
godwin opara <godwinopara62@yahoo.com>
2021-08-30 18:09:09 +0400

    finished styling the web design page

--- src/components/images.js
+++ src/components/images.js
@@ -33,17 +33,38 @@ import heroBackgroundMobile from "../images/contact/mobile/bg-pattern-hero-conta
 import iconError from "../images/contact/desktop/icon-error.svg";
 
 /* Location Page Images */
+
 import australiaMap from "../images/locations/desktop/image-map-australia.png";
 import canadaMap from "../images/locations/desktop/image-map-canada.png";
 import unitedKingdomMap from "../images/locations/desktop/image-map-united-kingdom.png";
 import australiaMapTablet from "../images/locations/tablet/image-map-australia.png";
 import canadaMapTablet from "../images/locations/tablet/image-map-canada.png";
 import unitedKingdomMapTablet from "../images/locations/tablet/image-map-uk.png";
+
 /* Graphic Design Images */
 
 import timBrownImg from "../images/graphic-design/desktop/image-change.jpg";
 import boxedWaterImg from "../images/graphic-design/desktop/image-boxed-water.jpg";
 import scienceImg from "../images/graphic-design/desktop/image-science.jpg";
+
+/* App Design Images */
+
+import appDesignIntoBackground from "../images/app-design/desktop/bg-pattern-intro-app.svg";
+import airfilterProjectImg from "../images/app-design/desktop/image-airfilter.jpg";
+import eyecamProjectImg from "../images/app-design/desktop/image-eyecam.jpg";
+import faceitProjectImg from "../images/app-design/desktop/image-faceit.jpg";
+import loopstudiosProjectImg from "../images/app-design/desktop/image-loopstudios.jpg";
+import todoProjectImg from "../images/app-design/desktop/image-todo.jpg";
+
+/* Web Design Images */
+
+import webDesignIntoBackground from "../images/web-design/desktop/bg-pattern-intro-web.svg";
+import blogrProjectImg from "../images/web-design/desktop/image-blogr.jpg";
+import builderProjectImg from "../images/web-design/desktop/image-builder.jpg";
+import campProjectImg from "../images/web-design/desktop/image-camp.jpg";
+import expressProjectImg from "../images/web-design/desktop/image-express.jpg";
+import photonProjectImg from "../images/web-design/desktop/image-photon.jpg";
+import transferProjectImg from "../images/web-design/desktop/image-transfer.jpg";
 
 /* Shared Images */
 
@@ -106,6 +127,25 @@ export const allImages = {
     timBrownImg,
     scienceImg,
     boxedWaterImg,
+  },
+
+  appDesignPage: {
+    appDesignIntoBackground,
+    airfilterProjectImg,
+    eyecamProjectImg,
+    faceitProjectImg,
+    loopstudiosProjectImg,
+    todoProjectImg,
+  },
+
+  webDesignPage: {
+    webDesignIntoBackground,
+    blogrProjectImg,
+    builderProjectImg,
+    campProjectImg,
+    expressProjectImg,
+    photonProjectImg,
+    transferProjectImg,
   },
 
   shared: {
