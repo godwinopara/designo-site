7d42aa913f8ec6d63e11dcd880ec7131da688ebc
godwin opara <godwinopara62@yahoo.com>
2021-08-30 20:04:34 +0400

    added background image to the hero of all the projects pages

--- src/pages/{mdx.slug}.js
+++ src/pages/{mdx.slug}.js
@@ -13,6 +13,10 @@ const Hero = styled.section`
   justify-content: center;
   margin-bottom: 9.6rem;
   color: ${({ theme }) => theme.primaryColors.white};
+  background-image: url(${({ mobileBackgroundImg }) => mobileBackgroundImg});
+  background-position: 40% 0;
+  background-repeat: no-repeat;
+  background-size: 220%;
 
   h1 {
     font-size: ${({ theme }) => theme.fontSize.large};
@@ -30,6 +34,10 @@ const Hero = styled.section`
     max-width: 1110px;
     margin: auto;
     border-radius: 15px;
+    background-image: url(${({ tabletBackgroundImg }) => tabletBackgroundImg});
+    background-position: center;
+    background-size: cover;
+
     h1 {
       font-size: ${({ theme }) => theme.fontSize.extraLarge};
       line-height: 48px;
@@ -39,14 +47,29 @@ const Hero = styled.section`
       width: 60%;
     }
   }
+
+  @media screen and (min-width: 1024px) {
+    background-image: url(${({ desktopBgImg }) => desktopBgImg});
+    background-size: 75%;
+    background-position: right;
+    background-repeat: no-repeat;
+  }
 `;
 
 const Projects = ({ data }) => {
-  console.log(data);
+  console.log(data.mdx.frontmatter);
   return (
     <Layout>
       <main>
-        <Hero>
+        <Hero
+          mobileBackgroundImg={
+            data.mdx.frontmatter.mobileBackgroundImg.publicURL
+          }
+          tabletBackgroundImg={
+            data.mdx.frontmatter.tabletBackgroundImg.publicURL
+          }
+          desktopBgImg={data.mdx.frontmatter.desktopBackgroundImg.publicURL}
+        >
           <div>
             <h1>{data.mdx.frontmatter.title}</h1>
             <p>{data.mdx.frontmatter.intro}</p>
@@ -63,12 +86,21 @@ export default Projects;
 export const guery = graphql`
   query ($id: String) {
     mdx(id: { eq: $id }) {
-      frontmatter {
-        title
-        intro
-      }
-      id
       body
+      id
+      frontmatter {
+        intro
+        title
+        desktopBackgroundImg {
+          publicURL
+        }
+        mobileBackgroundImg {
+          publicURL
+        }
+        tabletBackgroundImg {
+          publicURL
+        }
+      }
     }
   }
 `;
